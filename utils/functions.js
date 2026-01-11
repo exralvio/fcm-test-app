e006832fe3cb883660cd70ed05404accbcab2da7
Alvio Leegia <exralvio@gmail.com>
2026-01-12 04:39:30 +0700

    Adjust functions

--- utils/functions.js
+++ utils/functions.js
@@ -1,15 +1,16 @@
-'use strict';
 const crypto = require('crypto');
 
+function generateIdentifier() {
+  const baseString =
+    Date.now().toString(36) + Math.random().toString(36).substring(2, 10);
+  const uniquePart = crypto
+    .createHash('sha256')
+    .update(baseString)
+    .digest('hex')
+    .substring(0, 16);
+  return `fcm-msg-${uniquePart}`;
+}
+
 module.exports = {
-  generateIdentifier: () => {
-    const baseString =
-      Date.now().toString(36) + Math.random().toString(36).substring(2, 10);
-    const uniquePart = crypto
-      .createHash('sha256')
-      .update(baseString)
-      .digest('hex')
-      .substring(0, 16);
-    return `fcm-msg-${uniquePart}`;
-  },
+  generateIdentifier,
 };
